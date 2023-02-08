a8m/envsubst

commit 4932428dbd07d1c78277cac11af29e91101157c0

parse: allow default value expression starting with operators (#42)
Fixes #41

parse/lex.go
@@ -123,8 +123,8 @@ func (l *lexer) nextItem() item {
 // lex creates a new scanner for the input string.
 func lex(input string, noDigit bool) *lexer {
 	l := &lexer{
-		input: input,
-		items: make(chan item),
+		input:   input,
+		items:   make(chan item),
 		noDigit: noDigit,
 	}
 	go l.run()
@@ -172,7 +172,7 @@ Loop:
 				}
 				l.subsDepth++
 				l.emit(itemLeftDelim)
-				return lexSubstitution
+				return lexSubstitutionOperator
 			case isAlphaNumeric(r):
 				return lexVariable
 			}
@@ -204,9 +204,37 @@ func lexVariable(l *lexer) stateFn {
 	}
 	l.emit(itemVariable)
 	if l.subsDepth > 0 {
-		return lexSubstitution
+		return lexSubstitutionOperator
 	}
 	return lexText
+}
+
+// lexSubstitutionOperator scans a starting substitution operator (if any) and continues with lexSubstitution
+func lexSubstitutionOperator(l *lexer) stateFn {
+	switch r := l.next(); {
+	case r == '}':
+		l.subsDepth--
+		l.emit(itemRightDelim)
+		return lexText
+	case r == eof || isEndOfLine(r):
+		return l.errorf("closing brace expected")
+	case r == '+':
+		l.emit(itemPlus)
+	case r == '-':
+		l.emit(itemDash)
+	case r == '=':
+		l.emit(itemEquals)
+	case r == ':':
+		switch l.next() {
+		case '-':
+			l.emit(itemColonDash)
+		case '=':
+			l.emit(itemColonEquals)
+		case '+':
+			l.emit(itemColonPlus)
+		}
+	}
+	return lexSubstitution
 }
 
 // lexSubstitution scans the elements inside substitution delimiters.
@@ -222,23 +250,6 @@ func lexSubstitution(l *lexer) stateFn {
 		fallthrough
 	case r == '$':
 		return lexVariable
-	case r == '+':
-		l.emit(itemPlus)
-	case r == '-':
-		l.emit(itemDash)
-	case r == '=':
-		l.emit(itemEquals)
-	case r == ':':
-		switch l.next() {
-		case '-':
-			l.emit(itemColonDash)
-		case '=':
-			l.emit(itemColonEquals)
-		case '+':
-			l.emit(itemColonPlus)
-		default:
-			l.emit(itemText)
-		}
 	default:
 		l.emit(itemText)
 	}

parse/lex_test.go
@@ -1,8 +1,8 @@
 package parse
 
 import (
-	"testing"
 	"strings"
+	"testing"
 )
 
 type lexTest struct {
@@ -72,6 +72,28 @@ var lexTests = []lexTest{
 		{itemVariable, 0, "BAR"},
 		tColEquals,
 		{itemVariable, 0, "$BAZ"},
+		tRight,
+		{itemText, 0, " foo"},
+		tEOF,
+	}},
+	{"substitution-leading-dash-1", "bar ${BAR:--1} foo", []item{
+		{itemText, 0, "bar "},
+		tLeft,
+		{itemVariable, 0, "BAR"},
+		tColDash,
+		{itemText, 0, "-"},
+		{itemText, 0, "1"},
+		tRight,
+		{itemText, 0, " foo"},
+		tEOF,
+	}},
+	{"substitution-leading-dash-2", "bar ${BAR:=-1} foo", []item{
+		{itemText, 0, "bar "},
+		tLeft,
+		{itemVariable, 0, "BAR"},
+		tColEquals,
+		{itemText, 0, "-"},
+		{itemText, 0, "1"},
 		tRight,
 		{itemText, 0, " foo"},
 		tEOF,

parse/parse_test.go
@@ -63,6 +63,12 @@ var parseTests = []parseTest{
 	{"issue #1", "${hello:=wo_rld} ${foo:=bar_baz}", "wo_rld bar_baz", errNone},
 	{"issue #2", "name: ${NAME:=foo_qux}, key: ${EMPTY:=baz_bar}", "name: foo_qux, key: baz_bar", errNone},
 	{"gh-issue-8", "prop=${HOME_URL-http://localhost:8080}", "prop=http://localhost:8080", errNone},
+	// operators as leading values
+	{"gh-issue-41-1", "${NOTSET--1}", "-1", errNone},
+	{"gh-issue-41-2", "${NOTSET:--1}", "-1", errNone},
+	{"gh-issue-41-3", "${NOTSET=-1}", "-1", errNone},
+	{"gh-issue-41-4", "${NOTSET:==1}", "=1", errNone},
+
 	// bad substitution
 	{"closing brace expected", "hello ${", "", errAll},
 

commit 8ddcf9dd81d94a83989911eb087f3053290f3d00

parse: proper single char variable detection
Fixes #43, regression from #42

parse/lex.go
@@ -218,6 +218,8 @@ func lexSubstitutionOperator(l *lexer) stateFn {
 		return lexText
 	case r == eof || isEndOfLine(r):
 		return l.errorf("closing brace expected")
+	case isAlphaNumeric(r) && strings.HasPrefix(l.input[l.lastPos:], "${"):
+		return lexVariable
 	case r == '+':
 		l.emit(itemPlus)
 	case r == '-':

parse/lex_test.go
@@ -33,6 +33,12 @@ var lexTests = []lexTest{
 		{itemVariable, 0, "$hello"},
 		tEOF,
 	}},
+	{"single char var", "${A}", []item{
+		tLeft,
+		{itemVariable, 0, "A"},
+		tRight,
+		tEOF,
+	}},
 	{"2 vars", "$hello $world", []item{
 		{itemVariable, 0, "$hello"},
 		{itemText, 0, " "},

parse/parse_test.go
@@ -9,6 +9,7 @@ var FakeEnv = []string{
 	"FOO=foo",
 	"EMPTY=",
 	"ALSO_EMPTY=",
+	"A=AAA",
 }
 
 type mode int
@@ -68,6 +69,9 @@ var parseTests = []parseTest{
 	{"gh-issue-41-2", "${NOTSET:--1}", "-1", errNone},
 	{"gh-issue-41-3", "${NOTSET=-1}", "-1", errNone},
 	{"gh-issue-41-4", "${NOTSET:==1}", "=1", errNone},
+
+	// single letter
+	{"gh-issue-43-1", "${A}", "AAA", errNone},
 
 	// bad substitution
 	{"closing brace expected", "hello ${", "", errAll},